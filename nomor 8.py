c1087610287580adfde5f03ac544194aecbdedac
abrarfmahendra <72193713+abrarfmahendra@users.noreply.github.com>
2020-11-29 23:11:33 +0700

    Update nomor 8.py

--- nomor 8.py
+++ nomor 8.py
@@ -1,3 +1,19 @@
-def rataRataValue (x):
-    rata2 = sum(x.values())/len(x)
-    print(rata2)
+buah = { 'Apel'  : 5000, 'jeruk' : 8500,'Mangga': 7800,'Duku'  : 6500 }
+def rataratahargabuah(buah):
+    totalharga = 0
+    jumlah = 0
+
+    for r,y in buah.items():
+        totalharga += y
+        jumlah += 1
+
+    ratarata = totalharga/jumlah
+    return ratarata
+
+def rerataHargaFruit(buah):
+    harga = list(fruit.values())
+    rata = sum(harga)/len(harga)
+    return rerata
+
+x = rataratahargabuah(buah)
+print('Rata-rata harga satuan dari seluruh buah yang ada adalah ',x)
