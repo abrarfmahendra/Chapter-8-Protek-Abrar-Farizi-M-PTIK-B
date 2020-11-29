e69b31561f4e55ae3afd1f89338f5d057d68b3b5
abrarfmahendra <72193713+abrarfmahendra@users.noreply.github.com>
2020-11-29 23:09:47 +0700

    Update nomor 7.py

--- nomor 7.py
+++ nomor 7.py
@@ -1,3 +1,9 @@
-def maxValue (x):
-    nilaiMax = max(x, key=x.get)
-    print(nilaiMax)
+Listbuah = { 'Apel'  : 5000,'Jeruk' : 8500,'Mangga': 7800,'Duku'  : 6500 }
+def mostExpensive(dictionary) :  
+    key = list(dictionary.keys())
+    values = tuple(dictionary.values())
+    hargaExpensive = max(values)
+    indexHargaExpensive = values.index(hargaExpensive)
+    print(key[indexHargaExpensive])
+print('Harga buah yang satuannya paling mahal adalah :')
+mostExpensive(Listbuah)
